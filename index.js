7cd9af99eca75ba0ca833946754a7e9de2d235f2
UndisputedMaster <theundisputedmaster2k17@gmail.com>
2019-03-23 14:05:17 +0530

    First Commit :)

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const createError = require('http-errors');
 const express = require('express')
 const path = require('path')
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const PORT = process.env.PORT || 5000
+
+var Router = require('./routes/route');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(logger('dev'))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use(cookieParser())
+  .use('/', Router)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
